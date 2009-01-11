b8e34943c544cb146db717c1d294c5c8221c58a7
Simon Menke <simon@5xm.org>
2009-01-11 15:29:36 +0100

    Added json and xml to the errors controller

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,18 +1,35 @@
 class ErrorsController < ApplicationController
   
-  helper_method :error, :project
+  helper_method :errors, :error, :project
   
   before_filter :require_user
   before_filter :require_membership
   
+  def index
+    errors
+    respond_to do |format|
+      format.xml  { render :xml  => errors }
+      format.json { render :json => errors }
+    end
+  end
+  
   def show
     error
+    respond_to do |format|
+      format.html # render show.html.erb
+      format.xml  { render :xml  => error }
+      format.json { render :json => error }
+    end
   end
   
   private
   
   def project
     @project ||= current_user.projects.find(params[:project_id])
+  end
+  
+  def errors
+    @errors ||= project.reports.all
   end
   
   def error
