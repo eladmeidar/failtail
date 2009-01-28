7dc0d52bed6097adb3460a678b7615ed4c916d64
Simon Menke <simon@mrhenry.be>
2009-01-28 14:29:55 +0100

    Added some style

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,45 @@ module ApplicationHelper
     render options
   end
   
+  class MenuBlockBuilder
+    def initialize(template)
+      @template = template
+      @links = []
+    end
+    
+    def build(&block)
+      block.call(self)
+      l = @links.size - 1
+      @links.each_with_index do |(link, active), i|
+        classes = []
+        classes << 'active' if active
+        classes << 'first'  if i == 0
+        classes << 'last'   if i == l
+        classes = classes.join(' ')
+        @template.send(:concat, %{<li class="#{classes}">#{link}</li>})
+      end
+    end
+    
+    def to(name, url, *args, &conditions)
+      url = @template.send(:url_for, url)
+      conditions ||= lambda { @template.send(:request).path == url }
+      link = @template.send(:link_to, name, url, *args)
+      @links << [link, conditions.call]
+    end
+  end
+  
+  def menu_block(options={}, &block)
+    concat(tag("ul", options, true))
+    MenuBlockBuilder.new(self).build(&block)
+    concat(%{ </ul> })
+  end
+  
+  def actions(&block)
+    content_for :sidebar_actions do
+      concat('<div class="block"><h3>Actions</h3>')
+      menu_block(:class => 'navigation', &block)
+      concat('</div>')
+    end
+  end
+  
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,8 +1,30 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+var Theme = {
+  activate: function(name) {
+    window.location.hash = 'themes/' + name
+    Theme.loadCurrent();
+  },
 
-document.observe('dom:loaded', function(){
-  $$('.shadow').each(function(e){
-    e.wrap('div', { 'class': 'shadow-wrapper' });
+  loadCurrent: function() {
+    var hash = window.location.hash;
+    if (hash.length > 0) {
+      matches = hash.match(/^#themes\/([a-z0-9\-_]+)$/);
+      if (matches && matches.length > 1) {
+        $('#current-theme').attr('href', '/stylesheets/themes/' + matches[1] + '/style.css');
+      } else {
+        alert('theme not valid');
+      }
+    }
+  }
+}
+
+$(document).ready(function() {
+  Theme.loadCurrent();
+  $.localScroll();
+  $('.table :checkbox.toggle').each(function(i, toggle) {
+    $(toggle).change(function(e) {
+      $(toggle).parents('table:first').find(':checkbox:not(.toggle)').each(function(j, checkbox) {
+        checkbox.checked = !checkbox.checked;
+      })
+    });
   });
 });
