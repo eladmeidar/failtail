3b4374b9af3aafbf1df7eb42fa41775af30226be
Simon Menke <simon@mrhenry.be>
2009-01-30 11:34:49 +0100

    Added default capistrano files

---
+++ config/deploy.rb
@@ -0,0 +1,13 @@
+set :application, "failtale"
+
+ssh_options[:forward_agent] = true
+default_run_options[:pty] = true
+set :repository,  "git@github.com:mrhenry/errors.git"
+set :scm, "git"
+
+set :deploy_via, :remote_cache
+set :branch, "master"
+
+role :app, "failtale.be"
+role :web, "failtale.be"
+role :db,  "failtale.be", :primary => true
