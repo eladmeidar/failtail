60a7a92a75e0b250690eb5fb8de202e8d2f9f0e7
Yves Van Broekhoven <yvesvanbroekhoven@gmail.com>
2010-04-27 17:22:01 +0200

    Bundled

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source :gemcutter
+gem "cucumber"
+gem "factory_girl"
+gem "shoulda"
+gem "authlogic"
+gem "railsgarden-message_block"
+gem "haml"
+gem "will_paginate"
--- config/environment.rb
+++ config/environment.rb
@@ -19,15 +19,12 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
-  config.gem 'thoughtbot-factory_girl', :lib => 'factory_girl',
-    :source => 'http://gems.github.com'
-  config.gem 'thoughtbot-shoulda', :lib => 'shoulda',
-    :source => 'http://gems.github.com'
+  config.gem 'factory_girl'
+  config.gem 'shoulda'
   config.gem "authlogic"
-  config.gem 'railsgarden-message_block', :lib => 'message_block', :source => 'http://gems.github.com'
+  config.gem 'railsgarden-message_block'
   config.gem 'haml'
-  config.gem 'mislav-will_paginate', :lib => 'will_paginate',
-    :source => 'http://gems.github.com'
+  config.gem 'will_paginate'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
