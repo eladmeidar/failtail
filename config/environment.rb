7913ea6dc0b1ff8e0541dfd53e45c94579384004
Simon Menke <simon@mrhenry.be>
2010-04-28 10:35:47 +0200

    load message_block

--- config/environment.rb
+++ config/environment.rb
@@ -7,44 +7,15 @@ RAILS_GEM_VERSION = '2.3.2' unless defined? RAILS_GEM_VERSION
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
-  # Settings in config/environments/* take precedence over those specified here.
-  # Application configuration should go into files in config/initializers
-  # -- all .rb files in that directory are automatically loaded.
-
-  # Add additional load paths for your own custom dirs
-  # config.load_paths += %W( #{RAILS_ROOT}/extras )
-
-  # Specify gems that this application depends on and have them installed with rake gems:install
-  # config.gem "bj"
-  # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
-  # config.gem "sqlite3-ruby", :lib => "sqlite3"
-  # config.gem "aws-s3", :lib => "aws/s3"
   config.gem 'factory_girl'
   config.gem 'shoulda'
   config.gem "authlogic"
-  config.gem 'railsgarden-message_block'
+  config.gem 'railsgarden-message_block', :lib => 'message_block'
   config.gem 'haml'
   config.gem 'will_paginate'
-
-  # Only load the plugins named here, in the order given (default is alphabetical).
-  # :all can be used as a placeholder for all plugins not explicitly named
-  # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
-
-  # Skip frameworks you're not going to use. To use Rails without a database,
-  # you must remove the Active Record framework.
-  # config.frameworks -= [ :active_record, :active_resource, :action_mailer ]
-
-  # Activate observers that should always be running
-  # config.active_record.observers = :cacher, :garbage_collector, :forum_observer
-
-  # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-  # Run "rake -D time" for a list of tasks for finding time zone names.
+  
   config.time_zone = 'UTC'
-
-  # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-  # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
-  # config.i18n.default_locale = :de
-
+  
   config.action_controller.session = {
     :session_key => '_errors_session',
     :secret      => '729bdf4f8affcc1dc4cd4e5d8b7c6b8fc91e5341e8a486d5203ed62679e60f0b324f707a36b60c04deb17aa4308e51ebecb440fc92bbb929cc72d87b434d8c8e'
